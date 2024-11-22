73f8a2c5d2ba5c6be0e3cefb3078147051f41826
Daniel Lima <contatodlima25@gmail.com>
2024-11-22 17:27:11 -0300

    First commit

---
+++ TedTecnoligiasWeb/src/main/java/com/daniellima/TedTecnoligiasWeb/TedTecnoligiasWebApplication.java
@@ -0,0 +1,13 @@
+package com.daniellima.TedTecnoligiasWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedTecnoligiasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedTecnoligiasWebApplication.class, args);
+	}
+
+}
