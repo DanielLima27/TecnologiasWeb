aa74ecd24a4e5bb9ff95d384b98f955198034e8a
Daniel Lima <contatodlima25@gmail.com>
2024-11-25 23:17:25 -0300

    feat: Coloca o lombok na classe para abstração

--- TedTecnoligiasWeb/src/main/java/com/daniellima/TedTecnoligiasWeb/Models/UserModel.java
+++ TedTecnoligiasWeb/src/main/java/com/daniellima/TedTecnoligiasWeb/Models/UserModel.java
@@ -1,70 +1,26 @@
 package com.daniellima.TedTecnoligiasWeb.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "tb_usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    private Long id;
     private String name;
     private String email;
     private LocalDate dataNasc;
     private String password;
 
-    public UserModel(){
 
-    }
-
-    public UserModel(Long id, String name, String email, LocalDate dataNasc, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.dataNasc = dataNasc;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDataNasc() {
-        return dataNasc;
-    }
-
-    public void setDataNasc(LocalDate dataNasc) {
-        this.dataNasc = dataNasc;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
